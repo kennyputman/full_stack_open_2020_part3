37026a25465799b66227f819984f23a9c00067ed
Ken Putman <kbputman@gmail.com>
2020-04-15 17:46:41 -0500

    uibuild

---
+++ build/precache-manifest.9b98aa63c1c79828180729cc0a5ba72b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9421bbc0dc3e44b183a5ec239dc636",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da998ff863f75601c2d8",
+    "url": "/static/css/main.611f5904.chunk.css"
+  },
+  {
+    "revision": "d89a492d3e9441558789",
+    "url": "/static/js/2.b860935e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b860935e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da998ff863f75601c2d8",
+    "url": "/static/js/main.7cbf43a3.chunk.js"
+  },
+  {
+    "revision": "d19032c1263fa01ea41a",
+    "url": "/static/js/runtime-main.08633c47.js"
+  }
+]);
